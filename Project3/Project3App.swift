1064753d110d89691ef7e5410d7b50060a4292ed
Dave Gumba <davegumba@gmail.com>
2023-09-21 16:05:35 -0400

    Initial Commit

---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Dave Gumba on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
